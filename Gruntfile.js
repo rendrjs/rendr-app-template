24be4d39dbedc68b6350699f28aef3c81e7c3306
Spike Brehm <ocelot@gmail.com>
2013-03-30 12:29:20 -0700

    Updating to use grunt-rendr-stitch instead of airbnb/node-asset-compiler

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,9 @@ var rootDir = __dirname;
 var path = require('path');
 var assetCompiler = require('asset-compiler');
 
-var publicDir = rootDir + '/public';
-var vendorDir = rootDir + '/assets/vendor';
-var stylesheetsDir = rootDir + '/assets/stylesheets';
+var stylesheetsDir = 'assets/stylesheets';
+var rendrDir = 'node_modules/rendr';
+var rendrModulesDir = rendrDir + '/node_modules';
 
 module.exports = function(grunt) {
   // Project configuration.
@@ -41,51 +41,41 @@ module.exports = function(grunt) {
           return filename.slice(0, 2) !== '__';
         }
       }
+    },
+
+    rendr_stitch: {
+      compile: {
+        options: {
+          dependencies: [
+            rendrDir + '/assets/vendor/**/*.js',
+            rendrModulesDir + '/underscore/underscore.js',
+            rendrModulesDir + '/backbone/backbone.js',
+            rendrModulesDir + '/async/lib/async.js',
+            'assets/vendor/**/*.js'
+          ],
+          aliases: [
+            {from: rendrDir + '/client', to: 'rendr/client'},
+            {from: rendrDir + '/shared', to: 'rendr/shared'}
+          ]
+        },
+        files: [{
+          dest: 'public/mergedAssets.js',
+          src: [
+            'app/**/*.js',
+            rendrDir + '/client/**/*.coffee',
+            rendrDir + '/shared/**/*.coffee'
+          ]
+        }]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('grunt-rendr-stitch');
 
-  grunt.registerTask('bundle', bundle);
-
-  grunt.registerTask('compile', ['handlebars', 'bundle', 'stylus']);
+  grunt.registerTask('compile', ['handlebars', 'rendr_stitch', 'stylus']);
 
   // Default task(s).
   grunt.registerTask('default', ['compile']);
 };
-
-var modulesDir = rootDir + '/node_modules';
-var rendrDir = modulesDir + '/rendr';
-var rendrVendorDir = rendrDir + '/assets/vendor';
-var rendrModulesDir = rendrDir + '/node_modules';
-
-/**
-* Package javascript into a single mergedAssets.js file.  (if compileTemplates runs first, compiledTemplates.js
-* will be included in this merged file).  Store mergedAssets.js in public dir.
-*/
-function bundle() {
-  var done = this.async();
-  var options = {
-    // source directories
-    src: [
-      {type:'src', path: rootDir + '/app'}, // All of the files in app/
-      {type:'src', path: rendrDir + '/client', as:'/rendr/client'},  // copy client files to /rendr/client
-      {type:'src', path: rendrDir + '/shared', as:'/rendr/shared'}   // copy shared files to /rendr/shared
-    ],
-    // specific file dependencies
-    depend: [
-      {type:'depend', path:rendrVendorDir,  files:['jquery-1.8.2.min.js',
-                                                   'handlebars-runtime-1.0.rc.1.js']},
-      {type:'depend', path:rendrModulesDir, files:['/underscore/underscore.js',
-                                                   '/backbone/backbone.js',
-                                                   '/async/lib/async.js']},
-
-      {type:'depend', path:vendorDir, files:'all'}
-    ],
-    // where the newly created packaged file will go
-    destPath: publicDir,
-    destFile: 'mergedAssets.js'
-  };
-  assetCompiler.bundle(options, done);
-}
