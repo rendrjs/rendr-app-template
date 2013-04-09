01ad2454ede48a18760e0ecee5fbb55087403e45
Spike Brehm <ocelot@gmail.com>
2013-04-09 15:52:35 -0700

    Adding example of using 'lazy=true' to subviews

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,5 @@ module.exports = function(match) {
   match('repos/:owner/:name', 'repos#show');
   match('users'       ,       'users#index');
   match('users/:login',       'users#show');
+  match('users_lazy/:login',  'users#show_lazy');
 };
