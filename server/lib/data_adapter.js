969c31ab8bcdb2fc365adc09cd21c1a505f54e54
Spike Brehm <ocelot@gmail.com>
2013-04-09 17:18:01 -0700

    server/lib/data_adapter.coffee -> server/lib/data_adapter.js

---
+++ server/lib/data_adapter.js
@@ -0,0 +1,97 @@
+var utils = require('./utils'),
+    _ = require('underscore'),
+    url = require('url'),
+    request = require('request'),
+    debug = require('debug')('app:DataAdapter'),
+    inspect = require('util').inspect;
+
+module.exports = DataAdapter;
+
+function DataAdapter(options) {
+  this.options = options || {};
+}
+
+//
+// `req`: Actual request object from Express/Connect.
+// `api`: Object describing API call; properites including 'path', 'query', etc.
+//        Passed to `url.format()`.
+// `options`: (optional) Options.
+// `callback`: Callback.
+//
+DataAdapter.prototype.makeRequest = function(req, api, options, callback) {
+  var _this = this, start, end;
+
+  if (arguments.length === 3) {
+    callback = options;
+    options = {};
+  }
+
+  options = _.clone(options);
+  _.defaults(options, {
+    convertErrorCode: true,
+    allow4xx: false
+  });
+
+  api = this.apiDefaults(api);
+  start = new Date().getTime();
+  request(api, function(err, response, body) {
+    if (err) return callback(err);
+    end = new Date().getTime();
+
+    debug('%s %s %s %sms', api.method.toUpperCase(), api.url, response.statusCode, end - start);
+    debug('%s', inspect(response.headers));
+
+    if (options.convertErrorCode) {
+      err = _this.getErrForResponse(response, {allow4xx: options.allow4xx});
+    }
+    if (typeof body === 'string' && ~(response.headers['content-type'] || '').indexOf('application/json')) {
+      try {
+        body = JSON.parse(body);
+      } catch (e) {
+        err = e;
+      }
+    }
+    callback(err, response, body);
+  });
+};
+
+DataAdapter.prototype.apiDefaults = function(api) {
+  var urlOpts, basicAuth, authParts;
+
+  urlOpts = _.defaults(_.pick(api, 'protocol', 'port', 'query'), _.pick(this.options, ['protocol', 'port', 'host']));
+  urlOpts.pathname = api.path || api.pathname;
+
+  api = _.defaults(api, {
+    method: 'GET',
+    url: url.format(urlOpts)
+  });
+
+  if (api.body != null) {
+    api.json = api.body;
+  }
+
+  basicAuth = process.env.BASIC_AUTH;
+  if (basicAuth != null) {
+    authParts = basicAuth.split(':');
+    api.auth = {
+      username: authParts[0],
+      password: authParts[1],
+      sendImmediately: true
+    };
+  }
+
+  return api;
+};
+
+// Convert 4xx, 5xx responses to be errors.
+DataAdapter.prototype.getErrForResponse = function(res, options) {
+  var status, err;
+  status = +res.statusCode;
+  err = null;
+  if (utils.isErrorStatus(status, options)) {
+    err = new Error(status + " status");
+    err.status = status;
+    err.body = res.body;
+  }
+  return err;
+};
