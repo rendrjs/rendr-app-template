e25da96a63f9d907d16bda08a1a301471cb0289b
Barrett Cook <bcook@tagged.com>
2013-05-01 23:48:14 -0700

    Adding some sample mocha tests

---
+++ test/app/models/user.test.js
@@ -0,0 +1,11 @@
+var should = require("should");
+var User = require('../../../app/models/user');
+
+describe('User', function() {
+
+    it('should use login as the model id', function() {
+        var user = new User({ login:1000 });
+        user.id.should.equal(1000);
+    });
+
+});
