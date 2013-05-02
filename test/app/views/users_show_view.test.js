6df1ce733766ec344514d7586e55ff5fe4b5fd85
Barrett Cook <bcook@tagged.com>
2013-05-02 16:18:00 -0700

    Add users_show_view test

---
+++ test/app/views/users_show_view.test.js
@@ -0,0 +1,13 @@
+var should = require("should");
+var UsersShowView = require('../../../app/views/users_show_view');
+
+describe('UsersShowView', function() {
+
+    it('should have repos data in getTemplateData', function() {
+        var repos = { foo: 'bar' };
+        var view = new UsersShowView({ repos: repos });
+        var data = view.getTemplateData();
+        data.should.have.property('repos', repos);
+    });
+
+});
