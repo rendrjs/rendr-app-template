9bf61494d718b7c8852fdf45c601ca0390972e12
Barrett Cook <bcook@tagged.com>
2013-05-02 21:26:46 -0700

    Whitespace

--- test/app/models/user.test.js
+++ test/app/models/user.test.js
@@ -3,9 +3,9 @@ var User = require('../../../app/models/user');
 
 describe('User', function() {
 
-    it('should use login as the model id', function() {
-        var user = new User({ login:1000 });
-        user.id.should.equal(1000);
-    });
+  it('should use login as the model id', function() {
+    var user = new User({ login:1000 });
+    user.id.should.equal(1000);
+  });
 
 });
--- test/app/views/users_show_view.test.js
+++ test/app/views/users_show_view.test.js
@@ -3,11 +3,11 @@ var UsersShowView = require('../../../app/views/users_show_view');
 
 describe('UsersShowView', function() {
 
-    it('should have repos data in getTemplateData', function() {
-        var repos = { foo: 'bar' };
-        var view = new UsersShowView({ repos: repos });
-        var data = view.getTemplateData();
-        data.should.have.property('repos', repos);
-    });
+  it('should have repos data in getTemplateData', function() {
+    var repos = { foo: 'bar' };
+    var view = new UsersShowView({ repos: repos });
+    var data = view.getTemplateData();
+    data.should.have.property('repos', repos);
+  });
 
 });
