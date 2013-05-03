a0d13a9f5eed63ba10ab8976172516b82503562b
Spike Brehm <ocelot@gmail.com>
2013-05-02 23:32:25 -0700

    updating the data in tests to more closely match real use case

--- test/app/models/user.test.js
+++ test/app/models/user.test.js
@@ -4,8 +4,8 @@ var User = require('../../../app/models/user');
 describe('User', function() {
 
   it('should use login as the model id', function() {
-    var user = new User({ login:1000 });
-    user.id.should.equal(1000);
+    var user = new User({login: 'someusername'});
+    user.id.should.equal('someusername');
   });
 
 });
