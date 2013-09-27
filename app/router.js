cfee163a1c873758e7b148a3629d035031b47399
Spike Brehm <ocelot@gmail.com>
2013-09-27 00:06:47 -0700

    Add es5shim & es5sham for x-browser inheritance
    The lightest-weight way to achieve cross-browser class-style
    inheritance is to use `Object.create()`, polyfilled by `es5shim` and
    `es5sham`.
    
    The polyfill to `Object.create` doesn't support the second parameter,
    so set the constructor manually.

--- app/router.js
+++ app/router.js
@@ -4,7 +4,11 @@ var Router = module.exports = function Router(options) {
   BaseClientRouter.call(this, options);
 };
 
-Router.prototype.__proto__ = BaseClientRouter.prototype;
+/**
+ * Cross-platform inheritance, taking advantage of `es5shim` for IE.
+ */
+Router.prototype = Object.create(BaseClientRouter.prototype);
+Router.prototype.constructor = BaseClientRouter;
 
 Router.prototype.postInitialize = function() {
   this.on('action:start', this.trackImpression, this);
